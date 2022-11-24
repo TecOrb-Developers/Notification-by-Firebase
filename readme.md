fb45b7a3deb40387dcf1ae93e82f30442a284902
Aashutosh sharma <aashutosh.2021@tecorb.co>
2022-11-24 11:03:15 +0530

    Adding send Notification by Firebase

---
+++ readme.md
@@ -0,0 +1,15 @@
+## Setup Steps:
+`npm install`
+### local server
+`npm run start:dev`
+### prod build
+`npm run build`
+### prod build run
+`node dist/index.js`
+### install fcm-node
+`npm install fcm-node`
+### create firebase account
+### declare project name in firebase 
+### use firebase server key
+
+
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import path from 'path';
+import helmet from 'helmet';
+
+import express, { NextFunction, Request, Response } from 'express';
+import StatusCodes from 'http-status-codes';
+import 'express-async-errors';
+
+const app = express()
+
+
+
+// Export here and start in a diff file (for testing).
+export default app;
