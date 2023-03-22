55a91563c5bc00234077a93a668d095a884ba207
Aashutosh sharma <aashutosh.2021@tecorb.co>
2023-03-22 11:43:12 +0530

    update readme file

--- readme.md
+++ readme.md
@@ -1,15 +1,20 @@
+## Send and Receive Notification using Firebase in node js (Send Notification code  src/helper/send_notification) and (Receive Notification code  src/pu blic/firebase-messaging-sw)
+## Introduction
+`We can send and receive notification using firebase in node js`
+### create firebase account
+### declare project name in firebase 
+### use firebase server key
 ## Setup Steps:
 `npm install`
+### install fcm-node
+`npm install fcm-node`
 ### local server
 `npm run start:dev`
 ### prod build
 `npm run build`
 ### prod build run
 `node dist/index.js`
-### install fcm-node
-`npm install fcm-node`
-### create firebase account
-### declare project name in firebase 
-### use firebase server key
+
+
 
 
--- src/helper/send_notification.ts
+++ src/helper/send_notification.ts
@@ -1,20 +1,19 @@
-import axios from 'axios';
-
-function sendPushNotification(tokens, title, body, data) {
+// import FCM from 'fcm-node'
+function sendPushNotification(tokens:any, title:any, body:any, data:any) {
     var FCM = require('fcm-node');
     var serverKey = "gfrjdikcxvfkmc9eew9003w0909ewokfd,"; //server key
     var fcm = new FCM(serverKey);
-    let notification = {
+    let notification:any = {
         title: title,
         body: body,
         sound: 'https://res.cloudinary.com/tecorbssvan/video/upload/v1650879605/sounds/mixkit-happy-bells-notification-937_hghli0.wav',
     }
-    var message = {
+    var message:any = {
         to: tokens  
     };
         message.notification = notification
         message.data = data    
-    fcm.send(message, function (err, response) {
+    fcm.send(message, function (err:any, response:any) {
         if (err) {
             console.log("Something has gone wrong in sending push!", err);
         } else {
